505d4997f09af5b4968d72765c00f8bc7ca5f266
Fatmir Qelibari <lgit-kontakt@fatmir.qelibari.de>
2021-08-17 12:17:28 +0200

    Boolean casting to false values

---
+++ boolean.php
@@ -0,0 +1,10 @@
+<?php
+	$false_string = "0";
+	$second_string_value = ""; //empty string
+	$integer_zero = 0;
+	$floats_zero = 0.0;
+	$second_float_zero = -0.0;
+	$array = array();
+	$type_Null = NULL;
+	//This values are converted to false boolean values. 
+	//All other values are true
