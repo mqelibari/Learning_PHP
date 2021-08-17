f33b0e127073796f929f20af40e86580fee0aa98
Fatmir Qelibari <lgit-kontakt@fatmir.qelibari.de>
2021-08-17 16:12:27 +0200

    Add rule for string conversion of bools

--- string_conversion.php
+++ string_conversion.php
@@ -7,3 +7,10 @@
 	$sec_var = 5.7;
 	$sec_str = strval($sec_var);
 	echo gettype($str);
+
+	//special conversion
+	$bo = true;
+	$t_str = (string) $bo;
+	echo "This string has the value 1: {$t_str}\n";
+	$sec_bo = false;
+	echo "This string is empty : $sec_bo\n";
