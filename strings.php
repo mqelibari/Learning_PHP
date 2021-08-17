7c665ff6a0d9176b5cfb7fdfeacb512a1fe44c76
Fatmir Qelibari <lgit-kontakt@fatmir.qelibari.de>
2021-08-17 15:47:51 +0200

    Add rule vor simple var parsing and complex pars.

--- strings.php
+++ strings.php
@@ -2,4 +2,6 @@
 	$var = 5;
 	echo "Here its expand: $var"."\n";
 	echo 'Here its not expand: $var '.'\n';
-	
+	echo "Here its expand in line ${var}5"."\n";
+	/*Here its a error because the var name is changed
+	echo "Here its not acceptet $var5";	*/
