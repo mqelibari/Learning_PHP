85c4f15d3fe294a8ac9627cdbb8ab42d3466a611
Fatmir Qelibari <lgit-kontakt@fatmir.qelibari.de>
2021-08-17 15:50:10 +0200

    Add rule for negativ indexes in strings

--- strings.php
+++ strings.php
@@ -1,7 +1,12 @@
 <?php
-	$var = 5;
+	$var = "Halo";
 	echo "Here its expand: $var"."\n";
 	echo 'Here its not expand: $var '.'\n';
 	echo "Here its expand in line ${var}5"."\n";
 	/*Here its a error because the var name is changed
 	echo "Here its not acceptet $var5";	*/
+
+	//Negativ Indexes
+	if($var[-2] == "l"){
+		echo "true";
+	}
